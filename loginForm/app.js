49e300e84f88df883bf78fe4732ce255db21bb7c
Ozan <ozancangu24@gmail.com>
2023-03-10 17:48:01 +0300

    third commit

--- loginForm/app.js
+++ loginForm/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const welcomeText = document.querySelector('.welcome-text');
+console.log("kajdkad");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
