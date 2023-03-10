011cafa5770b25c24126805d0474e3fee5f16bfc
Ozan <ozancangu24@gmail.com>
2023-03-10 17:46:52 +0300

    sec commit

---
+++ loginForm/app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const welcomeText = document.querySelector('.welcome-text');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    welcomeText.textContent = `Welcome ${input.value}!`;
+    welcomeText.classList.add('show');
+    input.value = ''; // clear the input field
+    setTimeout(() => {
+        welcomeText.classList.remove('show');
+    }, 3000); // hide the text after 3 seconds
+});
